f922f58fc651b55481279bf53629bba404067e8f
hardik-agrl <hardik2001agarwal@gmail.com>
2023-11-27 19:44:34 +0530

    db changed

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,25 @@
-const mongoose = require('mongoose');
-const mongoUrl= 'mongodb://127.0.0.1/inotebook'
+const mongoose = require("mongoose");
+const mongoUrl = "mongodb://127.0.0.1/inotebook";
 // mongoose.connect('mongodb://127.0.0.1/test')
-const connectToMongo = ()=>{
-    mongoose.connect(mongoUrl)
-    .then(() => console.log('DB is connected'))
-    .catch(err => console.log(err));
 
-}
+const connectToMongo = () => {
+//   mongoose
+//     .connect(mongoUrl)
+//     .then(() => console.log("DB is connected"))
+//     .catch((err) => console.log(err));
+
+  mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+
+  const connection = mongoose.connection;
+
+  connection.on("error", (error) => {
+    console.error("MongoDB connection error:", error);
+  });
+
+  connection.once("open", () => {
+    console.log("Connected to MongoDB");
+  });
+};
 
 // // connectToMongo().catch(err => console.log(err));
 
@@ -15,4 +28,3 @@ const connectToMongo = ()=>{
 // }
 
 module.exports = connectToMongo;
-
