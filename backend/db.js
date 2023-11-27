e0f395ba673090dede5a4e81a89fea5953c34ee2
hardik-agrl <hardik2001agarwal@gmail.com>
2023-11-27 20:07:57 +0530

    port no changed

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 const mongoUrl = "mongodb://127.0.0.1/inotebook";
+// const mongoUrl = "mongodb://localhost:27017/inotebook";
+
 // mongoose.connect('mongodb://127.0.0.1/test')
 
 const connectToMongo = () => {
